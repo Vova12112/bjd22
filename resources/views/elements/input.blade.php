2b6c309db3a0a96906d278c24c600d1201de717c
Denis Gunyavyj <denis.gunyavy@gmail.com>
2022-03-30 03:15:07 +0300

    [TASK] Create organization page, add atributes for input

--- resources/views/elements/input.blade.php
+++ resources/views/elements/input.blade.php
@@ -1,5 +1,5 @@
 <div class="input-block">
-	<input type="text" class="default" placeholder=" " required id="{{ $inputId }}" value="{{ $value ?? '' }}">
+	<input type="text" class="default" placeholder=" " required id="{{ $inputId }}" value="{{ $value ?? '' }}" readonly="{{$isReadOnly}} disable="{{$isDisable}}">
 	<label for="{{ $inputId }}">{{ $label ?? '' }}</label>
 </div>
 
--- resources/views/pages/organization.blade.php
+++ resources/views/pages/organization.blade.php
@@ -1,8 +1,21 @@
 @extends('pages._common')
 @section('title')
-	Підприємство
+	Відомості про організацію
 @endsection
 
 @section('content')
-	Якийсь контент
+	<div class="conteiner">
+		<div class="orgName">
+			<p style="width: 140px">Назва фірми:</p>
+			<input type="text" class="name">
+		</div>
+		<div class="orgName">
+			<p style="width: 140px">Адреса:</p>
+			<input type="text" class="name2">
+		</div>
+		<div id="orgbtn">
+			<button class="btn">Зберегти</button>
+		</div>
+
+	</div>
 @endsection
