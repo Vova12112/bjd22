6079d250f85a4db5c5b0c5f4ae5b72894797f0e1
Anikin Vladimir <vladimir.a@droam.com>
2022-03-31 03:11:11 +0300

    [FIX] Rebasing errors were fixed

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,59 +4,28 @@
 @endsection
 
 @section('content')
+	<div class="container">
+		@include(
+		'elements.homeb', [
+			'title' => 'ПРАЦІВНИКИ',
+			'description' => 'Перелік працівників, додавання, редагування, звільнення.<br><br>Перелік посад та управління ними.',
+			'textbtn' => 'Перейти'
+			]
+		)
+		@include(
+		'elements.homeb', [
+			'title' => 'ПІДПРИЄМСТВО',
+			'description' => 'Перелік підприємств, додавання, редагування, видалення.<br><br>Перелік посад та управління ними.',
+			'textbtn' => 'Перейти'
+			]
+		)
+		@include(
+		'elements.homeb', [
+			'title' => 'ТРАВМАТИЗМ',
+			'description' => 'Перелік травм, додавання, редагування, видалення.<br><br>Перелік типів, видів, причин.',
+			'textbtn' => 'Перейти'
+			]
+		)
 
-	<div class="content-row action js-popup-noscript" data-popup="change-password-popup" data-popup-class="user-profile-popup password-popup js-change-password-popup">
-		<div class="row-wrapper d-flex-align-center">
-			<h4 class="row-cell right row-text">
-				CLICK HERE
-			</h4>
-		</div>
 	</div>
-	<noscript class="change-password-popup hidden" data-title="DAWDAWDAWDAWDD">
-		<form class="js-password p-20">
-			AWDASDWADWA
-			<div class="mt-20 w-100 tar">
-				<button class="btn primary-btn submit js-save" disabled>
-					SAVE
-				</button>
-			</div>
-		</form>
-		<script type="application/javascript">
-			$(document).ready(function () {
-				"use strict";
-				let
-					$mainPopup = popup.getMainPopup(),
-					$inputs = $mainPopup.find("input"),
-					$currentPasswordInput = $mainPopup.find("#js-current-password"),
-					$newPasswordInput = $mainPopup.find("#js-new-password"),
-					$confirmPasswordInput = $mainPopup.find("#js-confirm-password"),
-					$saveButton = $mainPopup.find(".js-save");
-
-				$currentPasswordInput.focus();
-
-				$inputs.on("keyup", function () {
-					enableButton(
-						$saveButton,
-						(
-							$currentPasswordInput.val() !== undefined && $currentPasswordInput.val() !== "" &&
-							$newPasswordInput.val() !== undefined && $newPasswordInput.val() !== "" &&
-							$confirmPasswordInput.val() !== undefined && $confirmPasswordInput.val() !== "" &&
-							$newPasswordInput.val() === $confirmPasswordInput.val()
-						)
-					);
-					clearInvalid($(this));
-				});
-
-				createSubmitHandler($saveButton);
-
-			});
-		</script>
-	</noscript>
-	<script type="text/javascript">
-		$(document).ready(function () {
-			"use strict";
-			noScriptPopupHandlers();
-			ajaxPopupHandlers(false);
-		});
-	</script>
 @endsection
