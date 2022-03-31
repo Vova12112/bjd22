fdbf0ffc1ac71ebacd397c02acbb78fe42f26a45
Denis Gunyavyj <denis.gunyavy@gmail.com>
2022-03-31 06:37:05 +0300

    [TASK] Create page structure segments details

--- resources/views/pages/structure_segments_details.blade.php
+++ resources/views/pages/structure_segments_details.blade.php
@@ -4,5 +4,20 @@
 @endsection
 
 @section('content')
-
+	<div class="own-workers-card-container">
+		<h3>Структурні підрозділи</h3>
+		@include(
+			'elements.input',
+			[
+				'inputId' => 'structure_segments_details',
+				'label' => 'Назва',
+				'value' => '',
+				'classes' => '',
+				'isReadOnly' => FALSE,
+				'isDisable' => FALSE,
+				'name'=>'structure-name'
+			]
+		)
+		<button class="btn structure segments details">Зберегти</button>
+	</div>
 @endsection
